42b328755705853c155b8bf9f877655ad8817700
Axel Magnuson <axelmagn@gmail.com>
2016-09-09 15:34:48 -0600

    Write simple client prompt that echos output

--- src/bin/irc-client.rs
+++ src/bin/irc-client.rs
@@ -2,6 +2,9 @@
 
 // Use the `env` module from the static library
 use std::env;
+use std::io::prelude::*;
+use std::io;
+use std::io::BufReader;
 
 // declare a usage string in static memory.
 const USAGE: &'static str = "Usage:
@@ -10,10 +13,22 @@ const USAGE: &'static str = "Usage:
 Connect to an irc host.
 ";
 
+const PROMPT: &'static str = "> ";
+
 fn main() {
     // parse arguments to retrieve host.
-    let host = env::args().nth(1).expect("No host was specified!");
-
-    // for now, just print the host and exit
+    let host = env::args().nth(1).expect(USAGE);
     println!("HOST:\t{}", host);
+
+    // echo repl
+    let stdin = BufReader::new(io::stdin()).lines();
+    let mut stdout = io::stdout();
+    print!("{}", PROMPT);
+    stdout.flush().unwrap();
+    for line in stdin {
+        let line = line.unwrap();
+        println!("{}", line);
+        print!("{}", PROMPT);
+        stdout.flush().unwrap();
+    }
 }
