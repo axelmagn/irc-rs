10f7e6356677ed3b4d6c6c6f0c70f8e40c238170
Axel Magnuson <axelmagn@gmail.com>
2016-09-10 22:58:10 -0600

    Write logging handler for server

--- src/bin/irc-server.rs
+++ src/bin/irc-server.rs
@@ -1,12 +1,33 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 
 fn handle_stream(stream: TcpStream) {
-    // TODO: do something
+    // log the connection
+    let name = format!("{}", stream.peer_addr().unwrap());
+    info!("CONNECTED\t{}", name);
+
+    // log each line
+    for line in BufReader::new(stream).lines() {
+        line.map(|line| info!("{}:\t{}", name, line))
+            .unwrap_or_else(|e| error!("ERROR ({})\t{}", name, e));
+    }
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:194").unwrap();
+    // set up logging
+    env_logger::init().unwrap();
+
+    // start listening on tcp
+    let listen_host = "127.0.0.1:3000";
+    let listener = TcpListener::bind(listen_host).unwrap();
+    info!("LISTENING\t{}", listen_host);
 
     for stream in listener.incoming() {
         match stream {
@@ -18,7 +39,7 @@ fn main() {
                 });
             },
             Err(e) => {
-                // TODO: log the error
+                error!("ERROR (server)\t{:?}", e);
             }
         }
     }
